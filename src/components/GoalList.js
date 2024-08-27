c5eada0a6a332a525cd92a478b73ff34e718cb9a
Vatsalmaheshbhai.chheta01@student.csulb.edu <Vatsal0315>
2024-08-27 14:06:42 -0700

    GoalList.js

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -1,6 +1,13 @@
 import React from "react";
 const GoalList = (props) => {
-    return
+    return(
+        <ul className="goal-list">
+            <li>Finish the Course</li>
+            <li>Learn React</li>
+            <li>Help other students in the Course QnA</li>
+        </ul>
+
+    );
 }
 
 export default GoalList;
